fca40663e2294f87bcc438b573c4ee5067ac447e
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-05 17:57:13 -0300

    Finalização do exercício

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+Console.WriteLine(" ||  || ");
+Console.WriteLine(" \\\\()// ");
+Console.WriteLine("//(__)\\\\");
+Console.WriteLine("||    ||");
+
+
+        }
+    }
+}
